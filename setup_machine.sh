93d3004978eb38baaa0cb52e5ec8f13dc3e2e58e
manish-j <manish.msiclub@gmail.com>
2017-12-01 16:24:26 +0530

    Create setup_machine.sh

---
+++ setup_machine.sh
@@ -0,0 +1,11 @@
+sudo -s
+cat alias update='apt-get update ' >> ~/.bashrc
+cat alias install='apt-get install -y ' >> ~/.bashrc
+cat alias pinstall='pip install ' >> ~/.bashrc
+source ~/.bashrc
+update
+install youtube-dl
+insall pip
+pip install pip -U
+pip install youtube-dl -U
+install nmap
